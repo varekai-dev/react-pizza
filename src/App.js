6ae8553805baf46ac7c10cd52e437599fa2d6914
Serhii <serhijsav@gmail.com>
2020-07-28 22:46:25 +0300

    fix error with redux

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import { Header } from './components'
-import { connect } from 'react-redux'
+import React, { useEffect, useState } from "react"
+import { Header } from "./components"
+import { connect } from "react-redux"
+import { Home, Cart } from "./pages"
+import { Route } from "react-router-dom"
+import axios from "axios"
 
-import { Home, Cart } from './pages'
-import { Route } from 'react-router-dom'
-import axios from 'axios'
-// import store from './redux/store'
-import { setPizzas } from './redux/actions/pizzas'
+import { setPizzas } from "./redux/actions/pizzas"
 
 class App extends React.Component {
   componentDidMount() {
-    axios.get('http://localhost:3000/db.json').then(({ data }) => {
-      // store.dispatch(setPizzas(data.pizzas))
+    axios.get("http://localhost:3000/db.json").then(({ data }) => {
+      window.store.dispatch(setPizzas(data.pizzas))
     })
   }
   render() {
-    // console.log(this.props)
-
     return (
       <div className="wrapper">
         <Header />
         <div className="content">
-          <Route exact path="/" render={() => <Home items={[]} />} />
+          <Route
+            exact
+            path="/"
+            render={() => <Home items={this.props.items} />}
+          />
           <Route exact path="/cart" component={Cart} />
         </div>
       </div>
@@ -31,7 +32,7 @@ class App extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-    items: state.pizzas.items
+    items: state.pizzas.items,
   }
 }
 
